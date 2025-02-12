816eeed2f82cc73c5cc81a3d6bb1ee2da65933e6
Gamal820 <160344688+Gamal820@users.noreply.github.com>
2025-02-12 22:42:14 +0200

    Add files via upload

---
+++ First_Task/Program.cs
@@ -0,0 +1,27 @@
+﻿using System; 
+
+namespace firstappcsharp 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            // طلب إدخال رقم من المستخدم
+            Console.WriteLine("Enter Number :");
+            // قراءة الرقم المدخل من المستخدم وتحويله إلى عدد صحيح
+            int n = int.Parse(Console.ReadLine());
+
+            // التحقق مما إذا كان الرقم زوجيًا
+            if (n % 2 == 0)
+            {
+                // إذا كان الرقم زوجيًا، طباعة النتيجة
+                Console.WriteLine($"{n} : is Even");
+            }
+            else
+            {
+                // إذا لم يكن الرقم زوجيًا، طباعة النتيجة
+                Console.WriteLine($"{n} : is Odd");
+            }
+        }
+    }
+}
